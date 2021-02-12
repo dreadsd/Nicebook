07551a44f79a13b3d9ad176f32665f05d2b03ae0
Erik Tejeda <etaeaes@protonmail.com>
2021-02-11 20:24:34 -0600

    Add body to posts

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_02_11_233626) do
   end
 
   create_table "posts", force: :cascade do |t|
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
