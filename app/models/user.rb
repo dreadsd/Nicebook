6643b96aa15e2e6cbce5f9a57f00b90b4745cb1c
Erik Tejeda <etaeaes@protonmail.com>
2021-02-11 20:29:00 -0600

    Add author ref to posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :friend_requests, -> { where(status: false) }, class_name: "Friendship", foreign_key: :friend_id
   has_many :friends, through: :friendships
 
+  has_many :posts, foreign_key: :author_id
+
   def send_request_to(user)
     request = Friendship.create(user: self, friend: user)
     user.friend_requests << request
