7a547b8b748e5d19e6666b4cb21a2ee7ebebd315
Erik Tejeda <etaeaes@protonmail.com>
2021-02-13 10:45:28 -0600

    Revert AddLikesToPosts

--- db/migrate/20210213154913_add_likes_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddLikesToPosts < ActiveRecord::Migration[6.1]
-  def change
-    add_column :posts, :likes, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_154913) do
+ActiveRecord::Schema.define(version: 2021_02_13_035509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2021_02_13_154913) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "author_id", null: false
-    t.integer "likes"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
