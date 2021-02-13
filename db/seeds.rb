f015f19e02e3b2d5c6b428b0e0cd89463b0d0f75
Erik Tejeda <etaeaes@protonmail.com>
2021-02-13 09:21:48 -0600

    Update posts index specs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-User.create(email: "john@doe.com", password: "johndoe01", password_confirmation: "johndoe01")
-User.create(email: "jane@doe.com", password: "janedoe01", password_confirmation: "janedoe01")
-User.create(email: "bob@rock.com", password: "bobrock01", password_confirmation: "bobrock01")
-User.create(email: "tito@poo.com", password: "titopoo01", password_confirmation: "titopoo01")
-User.create(email: "mama@mas.com", password: "mamamas01", password_confirmation: "mamamas01")
+User.create(name: "John Doe", email: "john@doe.com", password: "johndoe01")#, password_confirmation: "johndoe01")
+User.create(name: "Jane Doe", email: "jane@doe.com", password: "janedoe01")#, password_confirmation: "janedoe01")
+User.create(name: "Bob Rock", email: "bob@rock.com", password: "bobrock01")#, password_confirmation: "bobrock01")
+User.create(name: "Tito Poo", email: "tito@poo.com", password: "titopoo01")#, password_confirmation: "titopoo01")
+User.create(name: "Mama Mas", email: "mama@mas.com", password: "mamamas01")#, password_confirmation: "mamamas01")
 
 User.first.posts.create(body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.")
 User.first.posts.create(body: "Contrary to popular belief, Lorem Ipsum is not simply random text.")
--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "posts/index", type: :view do
   before(:each) do
-    user = User.create(email: "dummy@test.com", password: "12345678")
+    user = User.create(name: "Dummy test", email: "dummy@test.com", password: "12345678")
     sign_in user
     assign(:posts, [
       Post.create!(body: "Dummy", author: user),
@@ -12,6 +12,9 @@ RSpec.describe "posts/index", type: :view do
 
   it "renders a list of posts" do
     render
+
+    expect(rendered).to have_selector(".author-name", text: "Dummy test")
+    expect(rendered).to have_selector(".pub-at", text: "less than a minute")
     expect(rendered).to match(/Dummy/)
     expect(rendered).to match(/Test/)
   end
