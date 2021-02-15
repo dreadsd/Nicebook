fa1d4c646030aad292118b53f7d67a28ca9eb893
Erik Tejeda <etaeaes@protonmail.com>
2021-02-14 19:23:53 -0600

    Change like form to controller action

--- app/controllers/likings_controller.rb
+++ app/controllers/likings_controller.rb
@@ -1,22 +1,22 @@
 class LikingsController < ApplicationController
-  def create
-    @liking = Liking.new(liking_params)
+  before_action :authenticate_user!, only: :like
 
-    unless @liking.save
-      flash[:error] = "Could not like"
+  def like
+    if params[:user_id].to_i == current_user.id
+      @liking = Liking.new(user_id: params[:user_id].to_i, post_id: params[:post_id].to_i)
+
+      unless @liking.save
+        flash[:error] = "Could not like"
+      end
+      redirect_back(fallback_location: root_path)
+    else
+      not_found
     end
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @liking = Liking.find(params[:id])
     @liking.destroy
     redirect_back(fallback_location: root_path)
-  end
-
-  private
-
-  def liking_params
-    params.require(:liking).permit(:user_id, :post_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :posts, except: :new
-  resources :likings, only: [:create, :destroy]
+  resources :likings, only: :destroy
+  get "/like_post/:user_id/:post_id", to: "likings#like", as: "like_post"
   resources :friendships, only: :destroy
   get "/accept_request/:id", to: "friendships#accept", as: "accept_request"
   devise_for :users
