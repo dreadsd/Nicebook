a4f262ea310bff5bc729a64bb508a9286c255735
Erik Tejeda <etaeaes@protonmail.com>
2021-02-14 21:34:37 -0600

    Add friendship#unfriend

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -13,6 +13,20 @@ class FriendshipsController < ApplicationController
     end
   end
 
+  def unfriend
+    if params[:user_id].to_i == current_user.id
+      fst_frnd = Friendship.find_by(user_id: params[:user_id], friend_id: params[:friend_id])
+      scd_frnd = Friendship.find_by(user_id: params[:friend_id], friend_id: params[:user_id])
+      if !(fst_frnd.destroy && scd_frnd.destroy)
+        flash[:error] = "Could not unfriend friend"
+      end
+      redirect_back(fallback_location: root_path)
+    else
+      not_found
+    end
+  end
+
+
   def destroy
     @friendship = Friendship.find(params[:id])
     @friendship.destroy
