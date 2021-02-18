333b8eb0f1f1bbeccba22d366db78fadf1c63547
Erik Tejeda <etaeaes@protonmail.com>
2021-02-17 19:55:42 -0600

    Set nested attr when comment comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,13 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: "User"
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable, dependent: :destroy
+
+  before_save :set_nested,
+    if: Proc.new { commentable.instance_of?(Comment) }
+
+  private
+
+  def set_nested
+    self.nested = true
+  end
 end
